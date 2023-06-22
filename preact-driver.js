c72d0dd94d5a08bfef67e2837cfb3784cf238af4
Matthew Greenberg <mgreenbe@ucalgary.ca>
2023-06-22 10:57:44 -0400

    mutation observer tests for preact

--- preact-driver.js
+++ preact-driver.js
@@ -3,25 +3,34 @@ import { render, h, Fragment } from "/src/index.js";
 console.log("~~~~~~\nPreact\n~~~~~~");
 const parentDom = document.getElementById("preact-parent-dom");
 
-render(null, parentDom);
-
 // const vnode = "hi";
 // const vnode = h(Fragment);
 // const vnode = true;
 // render(vnode, parentDom);
-// const observer = new MutationObserver((muts) => {
-// 	for (let mut of muts) {
-// 		console.log(mut);
-// 	}
-// });
+const observer = new MutationObserver((muts) => {
+	for (let mut of muts) {
+		console.log(mut);
+	}
+});
 
-// observer.observe(parentDom, {
-// 	characterData: true,
-// 	characterDataOldValue: true,
-// 	attributes: true,
-// 	childList: true,
-// 	subtree: true,
-// });
+observer.observe(parentDom, {
+	characterData: true,
+	characterDataOldValue: true,
+	attributes: true,
+	childList: true,
+	attributeOldValue: true,
+	subtree: true,
+});
+
+render("Hello, world!", parentDom);
+render("Yo, dude!", parentDom);
+// let vnode = h("p", { className: "foo" }, "Hi, mom!");
+// render(vnode, parentDom);
+// vnode = h("p", { className: "foo" }, "Yo, dude!");
+// render(vnode, parentDom);
+
+// vnode = h("p", { className: "bar" });
+// render(vnode, parentDom);
 
 // let vnode = h(Fragment, {}, [
 // 	h("nav", {}, [h("a", { href: "/" }, "Home"), h("hr")]),
---
+++ preact.test.js
@@ -0,0 +1,321 @@
+import "@testing-library/jest-dom";
+import {
+	render,
+	createElement as h,
+	Fragment,
+} from "./node_modules/preact/dist/preact.mjs";
+import { setTimeout } from "timers/promises";
+
+const observeConfig = {
+	characterData: true,
+	characterDataOldValue: true,
+	attributes: true,
+	childList: true,
+	attributeOldValue: true,
+	subtree: true,
+};
+
+const objDefaults = {
+	addedNodes: new Set([]),
+	removedNodes: new Set([]),
+	oldValue: null,
+	attributeName: null,
+};
+
+function rec2Obj(x) {
+	const obj = {
+		type: x.type,
+		addedNodes: new Set(Array.from(x.addedNodes).map((x) => x.outerHTML)),
+		removedNodes: new Set(Array.from(x.removedNodes).map((x) => x.outerHTML)),
+		target: x.target instanceof Text ? x.target.data : x.target.outerHTML,
+		oldValue: x.oldValue,
+		attributeName: x.attributeName,
+	};
+	return obj;
+}
+
+test("change text content of root", async () => {
+	const c = document.createElement("div");
+
+	render(h(Fragment, {}, "Hello, world!"), c);
+
+	let count = 0;
+	const observer = new MutationObserver((x) => {
+		count++;
+		const y = x.map(rec2Obj);
+		const z = [
+			{
+				...objDefaults,
+				type: "characterData",
+				oldValue: "Hello, world!",
+				target: "Yo, dude!",
+			},
+		];
+		expect(new Set(y)).toEqual(new Set(z));
+	});
+	observer.observe(c, observeConfig);
+	render(h(Fragment, {}, "Yo, dude!"), c);
+	await setTimeout(17);
+	observer.disconnect();
+	expect(c.innerHTML).toBe(`Yo, dude!`);
+	expect(count).toBe(1);
+});
+
+test("change text content of p", async () => {
+	const c = document.createElement("div");
+
+	render(h("p", {}, "Hello, world!"), c);
+
+	let count = 0;
+	const observer = new MutationObserver((x) => {
+		count++;
+		const y = x.map(rec2Obj);
+		const z = [
+			{
+				...objDefaults,
+				type: "characterData",
+				oldValue: "Hello, world!",
+				target: "Yo, dude!",
+			},
+		];
+		expect(new Set(y)).toEqual(new Set(z));
+	});
+	observer.observe(c, observeConfig);
+	render(h("p", {}, "Yo, dude!"), c);
+	await setTimeout(17);
+	observer.disconnect();
+	expect(c.innerHTML).toBe(`<p>Yo, dude!</p>`);
+	expect(count).toBe(1);
+});
+
+test("change className of p", async () => {
+	const c = document.createElement("div");
+
+	render(h("p", { className: "foo" }), c);
+
+	let count = 0;
+	const observer = new MutationObserver((x) => {
+		count++;
+		const y = x.map(rec2Obj);
+		const z = [
+			{
+				...objDefaults,
+				type: "attributes",
+				attributeName: "class",
+				oldValue: "foo",
+				target: `<p class="bar"></p>`,
+			},
+		];
+		expect(new Set(y)).toEqual(new Set(z));
+	});
+	observer.observe(c, observeConfig);
+	render(h("p", { className: "bar" }), c);
+	await setTimeout(17);
+	observer.disconnect();
+	expect(c.innerHTML).toBe(`<p class="bar"></p>`);
+	expect(count).toBe(1);
+});
+
+test("rerender div to p", async () => {
+	const c = document.createElement("div");
+
+	render(h("div", { id: "one" }, "Hello, world!"), c);
+
+	let count = 0;
+	const observer = new MutationObserver((x) => {
+		count++;
+		const y = x.map(rec2Obj);
+		const z = [
+			{
+				...objDefaults,
+				type: "childList",
+				target: `<div><p id="two">Yo, dude!</p></div>`,
+				addedNodes: new Set([`<p id="two">Yo, dude!</p>`]),
+			},
+			{
+				...objDefaults,
+				type: "childList",
+				target: `<div><p id="two">Yo, dude!</p></div>`,
+				removedNodes: new Set([`<div id="one">Hello, world!</div>`]),
+			},
+		];
+		expect(new Set(y)).toEqual(new Set(z));
+	});
+	observer.observe(c, observeConfig);
+	render(h("p", { id: "two" }, "Yo, dude!"), c);
+	await setTimeout(17);
+	observer.disconnect();
+	// expect(c.innerHTML).toBe(`<p id="two">Yo, dude!</p>`);
+	// expect(count).toBe(1);
+});
+
+test("renders text", () => {
+	const c = document.createElement("div");
+	render("Hello, world!", c);
+	expect(c.innerHTML).toBe("Hello, world!");
+});
+
+test("renders a number", () => {
+	const c = document.createElement("div");
+	render(666.666, c);
+	expect(c.innerHTML).toBe("666.666");
+});
+
+test("renders a boolean", () => {
+	const c = document.createElement("div");
+	render(true, c);
+	expect(c.innerHTML).toBe("");
+});
+
+test("renders null", () => {
+	const c = document.createElement("div");
+	render(null, c);
+	expect(c.innerHTML).toBe("");
+});
+
+test("renders empty p tag", () => {
+	const div = document.createElement("div");
+	const p = document.createElement("p");
+	div.appendChild(p);
+
+	const c = document.createElement("div");
+	render(h("p"), c);
+	expect(c.innerHTML).toBe(div.innerHTML);
+});
+
+test("renders p tag with text content", () => {
+	const div = document.createElement("div");
+	const p = document.createElement("p");
+	p.textContent = "Hi, mom!";
+	div.appendChild(p);
+
+	let c = document.createElement("div");
+	render(h("p", {}, "Hi, mom!"), c);
+	expect(c.innerHTML).toBe(div.innerHTML);
+
+	c = document.createElement("div");
+	render(h("p", {}, ["Hi, mom!"]), c);
+	expect(c.innerHTML).toBe(div.innerHTML);
+});
+
+test("renders p tag with a className", () => {
+	const div = document.createElement("div");
+	const p = document.createElement("p");
+	p.classList.add("foo");
+	div.appendChild(p);
+
+	let c = document.createElement("div");
+	render(h("p", { className: "foo" }), c);
+	expect(c.innerHTML).toBe(div.innerHTML);
+});
+
+test("renders a simple list", () => {
+	let c = document.createElement("div");
+	const x = h("ul", {}, [
+		h("li", {}, "first"),
+		h("li", {}, "second"),
+		h("li", {}, "third"),
+	]);
+	render(x, c);
+	expect(c.innerHTML).toBe(
+		"<ul><li>first</li><li>second</li><li>third</li></ul>"
+	);
+});
+
+test("renders three nested divs", () => {
+	let c = document.createElement("div");
+	const x = h(
+		"div",
+		{ className: "outer" },
+		h("div", { className: "middle" }, h("div", { className: "inner" }))
+	);
+	render(x, c);
+	expect(c.innerHTML).toBe(
+		`<div class="outer"><div class="middle"><div class="inner"></div></div></div>`
+	);
+});
+
+test("renders an empty fragment", () => {
+	const c = document.createElement("div");
+	render(h(Fragment), c);
+	expect(c.innerHTML).toBe("");
+});
+
+test("renders nested empty fragments", () => {
+	const c = document.createElement("div");
+	render(h(Fragment, {}, h(Fragment, {}, h(Fragment))), c);
+	expect(c.innerHTML).toBe("");
+});
+
+test("renders nested fragments", () => {
+	const c = document.createElement("div");
+	render(
+		h(
+			Fragment,
+			{},
+			"outer",
+			h(Fragment, {}, h(Fragment, {}, "-middle-"), "inner")
+		),
+		c
+	);
+	expect(c.innerHTML).toBe("outer-middle-inner");
+});
+
+test("renders a fragment containing text", () => {
+	const c = document.createElement("div");
+	render(h(Fragment, {}, "Hello, world!"), c);
+	expect(c.innerHTML).toBe("Hello, world!");
+});
+
+test("renders a fragment containing elements and text", () => {
+	const x = h(Fragment, {}, [
+		h(
+			"div",
+			{ className: "outer" },
+			h("div", { className: "middle" }, h("div", { className: "inner" }))
+		),
+		"Hi, mom!",
+		h("ul", {}, [
+			h("li", {}, "first"),
+			h("li", {}, "second"),
+			h("li", {}, "third"),
+		]),
+	]);
+	const c = document.createElement("div");
+	render(x, c);
+	expect(c.innerHTML).toBe(
+		`<div class="outer"><div class="middle"><div class="inner"></div></div></div>Hi, mom!<ul><li>first</li><li>second</li><li>third</li></ul>`
+	);
+});
+
+test("renders a simple list with fragments not at top level", () => {
+	let c = document.createElement("div");
+	const x = h("ul", {}, [
+		h("li", {}, h(Fragment, {}, "first")),
+		h("li", {}, h(Fragment, {}, "second")),
+		h("li", {}, h(Fragment, {}, "third")),
+	]);
+	render(x, c);
+	expect(c.innerHTML).toBe(
+		"<ul><li>first</li><li>second</li><li>third</li></ul>"
+	);
+});
+
+test("simple site", () => {
+	let c = document.createElement("div");
+	let x = h("div", {}, [
+		h("nav", {}, [h("a", { href: "/" }, "Home"), h("hr")]),
+		h(
+			"article",
+			{
+				key: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
+			},
+			"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+		),
+		h("footer", {}, [h("hr"), h("p", {}, h("i", {}, "(c) Me 2023"))]),
+	]);
+	render(x, c);
+	expect(c.innerHTML).toBe(
+		`<div><nav><a href="/">Home</a><hr></nav><article>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</article><footer><hr><p><i>(c) Me 2023</i></p></footer></div>`
+	);
+});
